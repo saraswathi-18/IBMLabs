c49cd53117fc280cddfb0d4f29949aced0f85b9b
saraswathi-18 <60912808+saraswathi-18@users.noreply.github.com>
2020-02-25 10:19:26 +0530

    Add files via upload

---
+++ literalscollection.py
@@ -0,0 +1,8 @@
+fruits=['apple',"mango"]   #list
+num=(1,2,3)      #tuple
+alphabets={'a':'apple','b':'banana'}    #dict
+vowels={'a','e','i','o','u'}   #set
+print(fruits)
+print(num)
+print(alphabets)
+print(vowels)
